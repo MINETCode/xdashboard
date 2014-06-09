080b8de2ebe5b065c1f07d56aee44a6282832b36
Radhika <radishghosla@yahoo.com>
2014-06-09 11:50:58 +0000

    Changes to forms.py

--- forms.py
+++ forms.py
@@ -1,4 +1,32 @@
 from django import forms
 
+from xdashboard.models import School, Event, EventTeam, Product
+
 class SchoolForm(forms.ModelForm):
     class Meta:
+	model = School
+	fields = ('name')
+
+
+class EventTeamForm(forms.Form):
+
+    event = forms.BooleanField(default=False)
+    mem1 = forms.CharField(label="Member 1", max_length = 20)
+    mem2 = forms.CharField(label="Member 2", max_length = 20, required = False)
+
+    if event.label == 'Quizzing':
+	mem3 = forms.CharField(label="Member 3", max_length = 20, required = False)
+		
+class BaseEventTeamFormSet(BaseFormSet):
+
+    def __init__(self, *args, **kwargs):
+	super(BaseEventTeamFormSet, self).__init__(*args, **kwargs)
+	
+	for i in range(0, len(Event.EVENTS)):
+	    self.[i].fields['event'].label = Event.EVENTS[i][1]
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+	model = Product
+	fields = ('name', 'descr', 'upForAcq')
